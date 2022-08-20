867b0823b52c1dfedf2e8104bbb3ec9481a0ad50
Sasha <110453914+Qina-Yan@users.noreply.github.com>
2022-08-20 01:08:42 -0700

    Delete build_model.py

--- build_model.py
+++
@@ -1,8 +0,0 @@
-from transformer import Cost_Transformer
-from model import ToyModel
-import pickle
-
-model = LogisticModel()
-model.train()
-with open('models/model.pkl', 'wb') as f:
-	pickle.dump(model,f)
